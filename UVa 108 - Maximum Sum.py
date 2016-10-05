a03e1722505b709f65116a626001b1ee9b7213f8
Md. Rezwanul Haque <r.haque.249.rh@gmail.com>
2016-10-05 19:34:33 +0600

    Solution

---
+++ UVa 108 - Maximum Sum.py
@@ -0,0 +1,52 @@
+def Solve(N, mat):
+    maxSubRect = -127*100*100
+    arr = []
+
+    for i in range(N):
+        arr.append([0]*N)
+
+    arr[0][0] = mat[0][0]
+    for i in range(1,N):
+        arr[0][i] = arr[0][i-1] + mat[0][i]
+
+    for i in range(1,N):
+        row = [0]*N
+        row[0] = mat[i][0]
+        for j in range(1,N):
+            row[j] = row[j-1] + mat[i][j]
+        for j in range(N):
+            arr[i][j] = row[j] + arr[i-1][j]
+
+    for i in range(N):
+        for j in range(N):
+            for k in range(i,N):
+                for l in range(j,N):
+                    subRect = arr[k][l]
+                    if (i>0):
+                        subRect -= arr[i-1][l]
+                    if (j>0):
+                        subRect -= arr[k][j-1]
+                    if (i>0 and j>0):
+                        subRect += arr[i-1][j-1]
+                    maxSubRect = max(maxSubRect, subRect)
+    return maxSubRect
+
+if __name__ == '__main__':
+    N = int(input())
+    buff = []
+    while True:
+        try:
+            buff += map(int,input().split())
+        except:
+            break
+
+    i = j = 0
+    mat = [[None]*N for _ in range(N)]
+    for x in buff:
+        mat[i][j] = x
+        j+=1
+        if j == N:
+            j = 0
+            i += 1
+
+    print(Solve(N,mat))
